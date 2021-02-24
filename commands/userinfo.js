d85ff601ac186ba7ff9fbad8822d49d860c4e4f6
Glitch (hello-express) <none>
2021-02-24 18:12:07 +0000

    👲🐠 Checkpoint ./package.json:28980549/952 ./watch.json:28980549/6 ./commands/userinfo.js:28980549/2159

---
+++ commands/userinfo.js
@@ -0,0 +1,73 @@
+const { MessageEmbed } = require("discord.js")
+const moment = require("moment")
+moment.locale("pt-br")
+
+module.exports = {
+  name: "userinfo",
+  aliases: ["whois", "user"],
+  usage: "userinfo <MENTION>",
+  description: "Get advance stats of given person or yourself",
+  run: async (client, message, args) => {
+
+
+    let user;
+
+    if (!args[0]) {
+      user = message.member;
+    } else {
+
+
+      if (isNaN(args[0])) return message.channel.send(":x: ID invalido.")
+
+
+      user = message.mentions.members.first() || await message.guild.members.fetch(args[0]).catch(err => { return message.channel.send(":x: Não consegui encontrar esse usuario") })
+    }
+
+    if (!user) {
+      return message.channel.send(":x: Não consegui encontrar esse usuario.")
+    }
+
+    let embed = new MessageEmbed()
+      .setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
+
+    //ACTIVITY
+    let array = []
+    if (user.user.presence.activities.length) {
+
+      let data = user.user.presence.activities;
+
+      for (let i = 0; i < data.length; i++) {
+        let name = data[i].name || "None"
+        let xname = data[i].details || "None"
+        let zname = data[i].state || "None"
+        let type = data[i].type
+
+        array.push(`**${type}** : \`${name} : ${xname} : ${zname}\``)
+
+        if (data[i].name === "Spotify") {
+          embed.setThumbnail(`https://i.scdn.co/image/${data[i].assets.largeImage.replace("spotify:", "")}`)
+        }
+
+        embed.setDescription(array.join("\n"))
+
+      }
+    }
+
+      //EMBED COLOR BASED ON member
+      embed.setColor(user.displayHexColor === "#000000" ? "#ffffff" : user.displayHexColor)
+
+      //OTHER STUFF 
+      embed.setAuthor(user.user.tag, user.user.displayAvatarURL({ dynamic: true }))
+
+      //CHECK IF USER HAVE NICKNAME
+      if (user.nickname !== null) embed.addField("Nome", user.nickname)
+      embed.addField("Conta Criada em:", moment(user.user.createdAt).format("LLLL"))
+        .addField("Informações", `**ID**: \`${user.user.id}\`\n**Tag**: \`${user.user.discriminator}\``)
+
+      return message.channel.send(embed).catch(err => {
+        return message.channel.send("Erro : " + err)
+      })
+
+
+   }
+}
