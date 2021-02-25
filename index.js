2ed4fe94a7163298db12e3d0ca5418eb329c01cf
Glitch (hello-express) <none>
2021-02-25 02:06:00 +0000

    🦂🍄 Checkpoint ./index.js:28980549/108 ./watch.json:28980549/2

--- index.js
+++ index.js
@@ -32,4 +32,4 @@ client.on('message', message => {
 });
 
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
-console.log('zora está online')
+console.log(`[CONECTADA] ${client.user.tag} foi conectado com sucesso ao Discord.`)
