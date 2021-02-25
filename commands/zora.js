b76aeb83725848eafefac8328a6fe4b6dfcd5b68
Glitch (hello-express) <none>
2021-02-25 01:31:17 +0000

    🎷🐬 Checkpoint ./commands/zora.js:28980549/427

--- commands/zora.js
+++ commands/zora.js
@@ -1,3 +1,16 @@
 const require = ("discord.js")
+let answers = [
+'sei não hein',
+'faz todo sentido!',
+'sim',
+'não',
+'com certeza!',
+'por que você está fazendo essa pergunta?'
 
-  ket
+]
+
+let answer = answers[Math.floor(Math.random()*answers.length)]
+
+let msg = new Discord.MessageEmbed()
+
+
