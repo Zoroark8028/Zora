d113a585837341fb34256b63b469366138203776
Glitch (hello-express) <none>
2021-02-26 17:19:55 +0000

    🎑👽 Checkpoint ./commands/botinfo.js:28980549/587

---
+++ commands/botinfo.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (client, message, args) => {
+
+ const embed = new Discord.MessageEmbed()
+ .setColor("#ff0000")
+ .setTitle("Informações sobre a Zora Natasha!")
+ .setDescription(`
+ Oie, sou um simples bot brasileiro, que estou tentando mudar o discord deixando os servidores mais vivos!
+
+Programada em **Discord.js**
+
+Criador: ``Zoroark:
+
+Hospedada na [Glitch](https://glitch.com)
+
+
+`)
+
+
+ }
