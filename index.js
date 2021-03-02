210c6bdf0c413f3878054116d2760c9d6fdb3f9b
Glitch (hello-express) <none>
2021-03-02 17:49:53 +0000

    🐓🍶 Checkpoint ./.glitch-assets:28980549/525 ./index.js:28980549/248 ./watch.json:28980549/2 ./z.avatar:28980549/546

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const config = require("./config.json"); //Pegando o prefixo do bot para respost
 
 client.on("ready", () => {
 let avatar = [
-`https://cdn.glitch.com/93d6f45a-a737-47e8-8966-31894015e729%2F78801ae9-1e40-431e-b311-6c8e0b10f9ca.image.png?v=1614371274275`,  
+`https://cdn.glitch.com/93d6f45a-a737-47e8-8966-31894015e729%2F240e2116-1c26-40c8-ba0a-6556f9cb97cd.image.png?v=1614706766742`,  
 `https://cdn.glitch.com/93d6f45a-a737-47e8-8966-31894015e729%2F8806f87f-be13-4dd9-a60a-ccf686d37348.image.png?v=1614371381738`,
 `https://cdn.glitch.com/93d6f45a-a737-47e8-8966-31894015e729%2F9a79cc19-82f8-4c35-9772-2009aba4e4d1.image.png?v=1614373229732`
 ],
