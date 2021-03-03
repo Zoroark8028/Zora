05f03c0f4a6b61638bdf628a91aa360906484553
Glitch (hello-express) <none>
2021-03-03 18:18:28 +0000

    📻🚣 Checkpoint ./commands/convidar.js:28980549/539 ./watch.json:28980549/6

--- commands/convidar.js
+++ commands/convidar.js
@@ -4,7 +4,11 @@ module.exports.run = async (client, message, args) => {
 
  const embed = new Discord.MessageEmbed()
  .setColor("#ff0000")
- .setTitle("Me Adicione!")
-.setDescription(`Me ajude a ficar famosa(e entreter mais servidores,claro!`)
- 
- }
+ .setTitle("Hey, Me Adicione Em Seu Servidor!")
+.setDescription(`<:hihihi:763836612114513930> | Que tal me ajudar a ficar famosa? (e entreter mais servidores,é claro!) 
+me ajude me adicionando ao seu servidor!
+[Clique aqui](https://discord.com/api/oauth2/authorize?client_id=803373957738528778&permissions=2048&scope=bot) Para me adicionar em seu servidor! `)
+
+message.channel.send(embed)
+
+}
