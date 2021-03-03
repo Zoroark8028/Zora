563c51b667e5788a076c5f2e995df34e9d82263a
Glitch (zora) <none>
2021-03-03 20:41:07 +0000

    🌼🎗 Checkpoint ./commands/8ball.js:28980549/1004 ./watch.json:28980549/8

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js')
 
+module.ex
  let responses = [
-        "Yes",
-        "No",
-        "Definetly",
-        "Absoloutely",
-        "Not in a million years",
+        "Sim",
+        "Não",
+        "Muito interresante",
+        "Que tal você ir estudar?",
+        "Não, Não,Não"
       ];
       let response =
         responses[Math.floor(Math.random() * responses.length - 1)];
-      let embed = new Discord.MessageEmbed()
+      const embed = new Discord.MessageEmbed()
        .setColor("#ff0000")
  .setTitle("8ball")
- .setDescription(`${response}`)
+ .setDescription(`${response}`)ports.run = async (client, message, args) => {
+if(!args[1]) return message.reply("Por favor coloque uma pergunta");
+
 message.channel.send(embed)
 }
