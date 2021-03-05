3a1d7332dfdea9685862d1de03494150f627e55a
Glitch (zora) <none>
2021-03-05 01:51:20 +0000

    👨📖 Checkpoint ./commands/zora.js:28980549/228 ./watch.json:28980549/4

--- commands/zora.js
+++ commands/zora.js
@@ -4,22 +4,21 @@ module.exports.run = async (client, message, args) => {
 
 
   let responses = [
-        "Sim",
-        "Não",
-        "Muito interresante",
-        "Que tal você ir estudar?",
-        "Não, Não,Não",
-'Zora Aprova!',
-    'Faz sentido.',
-    'hm, pensando bem, eu nunca pensei nisto!'
-      ];
+        "<:hihihi:763836612114513930>・Sim",
+        "<:Leon_Shark_Triste:807662645326905366>・Não",
+        "・Muito interresante",
+        "・Que tal você ir estudar?",
+        "・Não, Não,Não",
+"・Zora Aprova!",
+   "・Faz sentido.",
+            "・hm, pensando bem, eu nunca pensei nisto!"];
       let response =
         responses[Math.floor(Math.random() * responses.length - 1)];
       const embed = new Discord.MessageEmbed()
        .setColor("#ff0000")
  .setTitle("Zora Responde!")
  .setDescription(`Para: ${message.author.tag},
-<:hihihi:763836612114513930> ・ **${response}**`)
+**${response}**`)
 if(!args[0]) return message.reply("Por favor coloque uma pergunta");
 
 message.channel.send(embed)
