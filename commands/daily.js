41393454e4b0e917458ed5a85a83dfa7d22f66ec
Glitch (zora) <none>
2021-03-05 03:01:41 +0000

    🌉😜 Checkpoint ./commands/teste.js:28980549/946 ./watch.json:28980549/8 ./package.json:28980549/980 ./commands/daily.js:28980549/1090

---
+++ commands/daily.js
@@ -0,0 +1,26 @@
+const db = require('quick.db');
+const ms = require('parse-ms');
+
+module.exports = {
+    name: "daily",
+    description: "Receive a daily award of money",
+
+    async run (client, message, args) {
+        let user = message.author;
+        let timeout = 86400000;
+        let amount = 500;
+
+        let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
+
+        if(daily !== null && timeout - (Date.now() - daily) > 0){
+            let time = ms(timeout - (Date.now() - daily));
+
+            return message.channel.send(`Você ja coletou seu daily de hoje, resgate denovo em: ${time.hours}horas , ${time.minutes}minutos e ${time.seconds}s`)
+        } else {
+            db.add(`money_${message.guild.id}_${user.id}`, amount);
+            db.set(`daily_${message.guild.id}_${user.id}`, Date.now());
+
+            message.channel.send(`Successfully added ${amount} coins to your account`)
+        }
+    }
+}
