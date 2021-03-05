5e7d7c87d7a09c95b7be16e418eaa90998c1aa39
Glitch (zora) <none>
2021-03-05 02:03:23 +0000

    🎢🎅 Checkpoint ./commands/zora.js:28980549/245 ./watch.json:28980549/6

--- commands/zora.js
+++ commands/zora.js
@@ -6,10 +6,10 @@ module.exports.run = async (client, message, args) => {
   let responses = [
         "<:hihihi:763836612114513930>・Sim",
         "<:Leon_Shark_Triste:807662645326905366>・Não",
-        "・Muito interresante",
-        "・Que tal você ir estudar?",
-        "・Não, Não,Não",
-"・Zora Aprova!",
+        "<:Sandy_Joinha:807660320700760115>・Muito interresante",
+        "<:lanchando:760219665933795409>・Que tal você ir estudar?",
+        "<:Bibi_Raiva:807663391144935484>・Não, Não,Não",
+"<:Shelly_Bruxa_Feliz:807667755885133884>・Zora Aprova!",
    "・Faz sentido.",
             "・hm, pensando bem, eu nunca pensei nisto!"];
       let response =
@@ -19,7 +19,7 @@ module.exports.run = async (client, message, args) => {
  .setTitle("Zora Responde!")
  .setDescription(`Para: ${message.author.tag},
 **${response}**`)
-if(!args[0]) return message.reply("Por favor coloque uma pergunta");
+if(!args[0]) return message.reply("<:Bibi_Raiva:807663391144935484> | Por favor, coloque uma pergunta");
 
 message.channel.send(embed)
 }
