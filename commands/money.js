6162f7a58d08121761511ff7b95ca880570a049b
Glitch (zora) <none>
2021-03-16 19:12:49 +0000

    🚅💰 Checkpoint ./commands/bal.js:28980549/117 ./watch.json:28980549/4 ./commands/money.js:28980549/562

--- commands/money.js
+++ commands/money.js
@@ -13,7 +13,7 @@ let user = message.member;
         let bal = await db.fetch(`zm_${message.guild.id}_${user.id}`);
         if(bal === null) bal = 0;
 
-       message.channel.send(`${message.author.tag}, você tem **${bal} ZoraMoedas**`)  
+       message.channel.send(`${message.author}, você tem **${bal} ZoraMoedas**`)  
     
     }
 }
