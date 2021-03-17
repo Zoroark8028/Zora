f7a533f073632b5b2750bc7b2fca387f7b4d9f84
Glitch (zora) <none>
2021-03-17 20:13:49 +0000

    😚🏹 Checkpoint ./commands/sugestão.js:28980549/153 ./watch.json:28980549/2

--- commands/sugestão.js
+++ commands/sugestão.js
@@ -5,22 +5,22 @@ message.delete();
 const content = args.join(" ");
 
 if (!args[0]) {
-  return message.channel.send(`${message.author.username}, escreva a sugestão após o comando`)
+  return message.channel.send(`${message.author.username}, *eu acho que você esqueceu de escrever algo*`)
 } else if (content.length > 600) {
   return message.channel.send(`${message.author.username}, Coloque uma sugestão de no maximo 600 letras.`);
 } else {
-  var canal = message.guild.channels.cache.find(ch => ch.id === "699780010235527278");
+  var canal = message.guild.channels.cache.find(ch => ch.id === "806023887149465610");
   const msg = await canal.send(
     new Discord.MessageEmbed()
     .setColor("blue")
-    .addField("Autor:", message.author)
-    .addField("Conteúdo", content)
-    .setFooter("ID do Autor: " + message.author.id)
+    .addField("Autor(a):", message.author)
+    .addField("Sugestão", content)
+    .setFooter("ID do Autor(a): " + message.author.id)
     .setTimestamp()
   );
   await message.channel.send(`${message.author} a sugestão foi enviada com sucesso!`);
 
-  const emojis = ["👍", ""];
+  const emojis = ["👍", "👎"];
 
   for (const i in emojis) {
     await msg.react(emojis[i])
