fcbba6da0bb140f31b0cd8362316095cd8d5c994
Glitch (zora) <none>
2021-03-17 20:24:56 +0000

    💏🐒 Checkpoint ./commands/sugestão.js:28980549/30 ./watch.json:28980549/2

--- commands/sugestão.js
+++ commands/sugestão.js
@@ -6,13 +6,13 @@ const content = args.join(" ");
 
 if (!args[0]) {
   return message.channel.send(`${message.author.username}, *eu acho que você esqueceu de escrever algo*`)
-} else if (content.length > 600) {
-  return message.channel.send(`${message.author.username}, Coloque uma sugestão de no maximo 600 letras.`);
+} else if (content.length > 1000) {
+  return message.channel.send(`${message.author.username}, Coloque uma sugestão de no maximo 1000 letras.`);
 } else {
   var canal = message.guild.channels.cache.find(ch => ch.id === "806023887149465610");
   const msg = await canal.send(
     new Discord.MessageEmbed()
-    .setColor("blue")
+    .setColor("BLUE")
     .addField("Autor(a):", message.author)
     .addField("Sugestão", content)
     .setFooter("ID do Autor(a): " + message.author.id)
