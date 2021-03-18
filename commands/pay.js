d66dbe6dd2d19f8090d9780cd842bcffe9da1a07
Glitch (zora) <none>
2021-03-18 21:08:08 +0000

    📍🔮 Checkpoint ./commands/pay.js:28980549/491 ./watch.json:28980549/10 ./commands/bal.js:28980549/9

--- commands/pay.js
+++ commands/pay.js
@@ -10,37 +10,38 @@ module.exports.run = async (bot, message, args) => {
   let member = db.fetch(`zm_${message.guild.id}_${message.author.id}`)
 
   let embed1 = new Discord.MessageEmbed()
-  .setColor("#FFFFFF")
+  .setColor("RED")
   .setDescription(`mencione alguem`);
 
   if (!user) {
       return message.channel.send(embed1)
   }
   let embed2 = new Discord.MessageEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`especifique a quantidade de zoramoedas`);
-  
+  .setColor("RED")
+  .setDescription(`Usagem Correta
+z.pay {menção} {quantidade}`);
+ 
   if (!args[1]) {
       return message.channel.send(embed2)
   }
   let embed3 = new Discord.MessageEmbed()
   .setColor("#FFFFFF")
-  .setDescription(`sei la powa`);
+  .setDescription(`Você não pode usar dinheiro negativo!`);
 
   if (message.content.includes('-')) { 
       return message.channel.send(embed3)
   }
   let embed4 = new Discord.MessageEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`você preguiça nao tem money`);
+  .setColor("RED")
+  .setDescription(`Você não tem ZoraMoedas o suficiente!`);
 
   if (member < args[1]) {
       return message.channel.send(embed4)
   }
 
-  let embed5 = new https://www.youtube.com/watch?v=jy_LWMQvCZw
-  .setColor("#FFFFFF")
-  .setDescription(`Você pagou ${user.user.name} ${args[1]} zm`);
+  let embed5 = new Discord.MessageEmbed()
+  .setColor("GREEN")
+  .setDescription(`Você deu ${args[1]} **ZoraMoedas** para ${user.user.username}.`);
 
   message.channel.send(embed5)
   db.add(`zm_${message.guild.id}_${user.id}`, args[1])
@@ -50,5 +51,5 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name:"pay",
-  aliases: [""]
+  aliases: ["pagar"]
 }
