4bbd3082e2fd0449cbe7c38a8ae8a303ece11a6a
Glitch (zora) <none>
2021-03-18 22:16:47 +0000

    📒🐄 Checkpoint ./commands/leaderboard.js:28980549/1694 ./watch.json:28980549/18

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,38 +2,21 @@ const Discord = require('discord.js')
 const db = require('quick.db')
 
 module.exports.run = async (bot, message, args) => {
-    if(!message.content.startsWith('z.'))return;  
 
-    const embed = new Discord.MessageEmbed()
-    .setDescription(`**PAra ver o leaderboard de **ZoraMoedas**\n\nCoin Leaderboard: m!leaderboard coins`)
-    .setColor("#FFFFFF")
+let user = message.mentions.members.first() || message.author
+let money = db.startsWith(`money_${user.id}_${message.guild.id}`, { sort: '.data' })
+let content = "";
 
+for (let i = 0; i < money.length; i++) {
+    let user = bot.users.get(money[i].ID.split('_')[1]).username
 
-  if(!args[0]) return message.channel.send(embed)
-
-    if (args[0] == 'moedas') {
-    let money = db.startsWith(`zm_${message.guild.id}`, { sort: '.data'})
-    let content = "";
-
-    for (let i = 0; i < money.length; i++) {
-        let user = bot.users.get(money[i].ID.split('_')[2]).username
-
-      
-
-        content += `${i+1}. ${user} ~ ${money[i].data}\n`
-    
-      }
-
-    const embed = new Discord.MessageEmbed()
-    .setDescription(`**${message.guild.name}'s Coin Leaderboard**\n\n${content}`)
-    .setColor("#FFFFFF")
-
-      }
-
-    message.channel.send(embed)
-  
+    content += `${i+1}. ${user} ~ ${money[i].data} snb\n`
 }
-module.exports.help = {
-  name:"leaderboard",
-  aliases: ["leader"]
+
+const embed = new Discord.RichEmbed()
+.setAuthor(`${message.guild.name} - Top players!`, message.guild.iconURL)
+.setDescription(content)
+.setColor(2686950)
+
+message.channel.send(embed)
 }
