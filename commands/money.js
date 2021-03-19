ff1ce201419e1aafdad549cc89249c42ae1b214c
Glitch (zora) <none>
2021-03-19 02:18:03 +0000

    🏭❄️ Checkpoint ./commands/money.js:28980549/655 ./watch.json:28980549/10 ./commands/addzm.js:28980549/407

--- commands/money.js
+++ commands/money.js
@@ -8,13 +8,15 @@ module.exports = {
     async run (client, message, args) {
 
 let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-   
-      if (!args[0]) return message.channel.send(`Mencione um usuário!`)
+
+if (!args[0]) {
+  return message.channel.send(`${message.author}, *eu acho que você esqueceu de mencionar alguem*`)
 
         let bal = await db.fetch(`zm_${message.guild.id}_${user.id}`);
         if(bal === null) bal = 0;
 
        message.channel.send(`${user}, tem **${bal} ZoraMoedas**`)  
     
+    }
     }
 }
