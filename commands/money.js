48738c85540d9295ef24aae38578f76787661297
Glitch (zora) <none>
2021-03-19 02:27:58 +0000

    👟💈 Checkpoint ./commands/money.js:28980549/276 ./watch.json:28980549/2

--- commands/money.js
+++ commands/money.js
@@ -7,10 +7,9 @@ module.exports = {
 
     async run (client, message, args) {
 
-let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+if (!args[0]) { return message.channel.send(`${message.author}, *eu acho que você esqueceu de mencionar alguem*`)
 
-if (!args[0]) {
-  return message.channel.send(`${message.author}, *eu acho que você esqueceu de mencionar alguem*`)
+let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
         let bal = await db.fetch(`zm_${message.guild.id}_${user.id}`);
         if(bal === null) bal = 0;
@@ -18,5 +17,5 @@ if (!args[0]) {
        message.channel.send(`${user}, tem **${bal} ZoraMoedas**`)  
     
     }
-    }
+   }
 }
