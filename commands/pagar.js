4a731a5b609cda1454782d36a5fef0424b46db04
Glitch (zora) <none>
2021-03-21 16:41:28 +0000

    🎭🗻 Checkpoint ./commands/pagar.js:28980549/1 ./watch.json:28980549/2

--- commands/pagar.js
+++ commands/pagar.js
@@ -24,7 +24,7 @@ module.exports.run = async (bot, message, args) => {
   if (!args[1]) {
       return message.channel.send(embed2)
 
-if (isNaN(args[1])) return message.channel.send(`**Isso não é um número, tá me fazendo de palhaça, é?!** :rage:`)
+if (isNaN(args[1])) return message.channel.send(`**Isso não é um número, tá me fazendo de palhaça, é?!** :rage:`);
 
 }
     let embed3 = new Discord.MessageEmbed()
