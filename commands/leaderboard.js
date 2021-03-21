14a2b1c9f9c81e4cb35653f2b089fbf581a65c4e
Glitch (zora) <none>
2021-03-21 16:53:12 +0000

    🌋🖖 Checkpoint ./commands/leaderboard.js:28980549/1334 ./watch.json:28980549/8

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -4,17 +4,17 @@ const db = require('quick.db')
 module.exports.run = async (bot, message, args) => {
 
 let user = message.mentions.members.first() || message.author
- let zm = db.all().filter(data => data.ID.startsWith(`zm`)).sort((a, b) => b.data - a.data)
-    zm.length = 10;
-    var finalLb = "";
-    for (var i in zm) {
-      finalLb += `**${zm.indexOf(zm[i])+1}. <@${bot.users.cache.get(zm[i].ID.split('_')[1]) ? bot.users.cache.get(zm[i].ID.split('_')[1]).tag : "User#0000"}>** - ${zm[i].data} :dollar:\n`;
-    }
-    const embed = new Discord.MessageEmbed()
-    .setAuthor(`Leaderboard!`, message.guild.iconURL())
-    .setColor("#7289da")
-    .setDescription(finalLb)
-    .setFooter(bot.user.tag, bot.user.displayAvatarURL())
-    .setTimestamp()
-    message.channel.send(embed);
+let money = db.all().filter(data => data.ID.startsWith(`money`)).sort((a, b) => b.data - a.data)
+        money.length = 10;
+        let finalLb = "";
+        for (var i in money) {
+          finalLb += `**${money.indexOf(money[i])+1}.**     <@${money[i].ID.slice(25)}> - \`${money[i].data} €\`\n`;
+        }
+        
+        
+        const embed = new Discord.MessageEmbed()
+        .setAuthor(`${message.guild.name}`, message.guild.iconURL({ dynamic: true }))
+        .setColor("#ff0000")
+        .addField(`Leaderboards`, finalLb, false)
+        message.channel.send(embed);
 }
