2c3d4bf82c8e9bb44604f96e7e1bfc7af09b97fc
Glitch (zora) <none>
2021-03-21 17:15:57 +0000

    🐫🌱 Checkpoint ./commands/teste.js:28980549/834 ./watch.json:28980549/10 ./commands/leader2.js:28980549/1020

---
+++ commands/leader2.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+module.exports.run = async (client, message, args) => {
+
+ let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+
+let money = db.all().filter(data => data.ID.startsWith(`money`)).sort((a, b) => b.data - a.data)
+        money.length = 10;
+        let finalLb = "";
+        for (var i in money) {
+          finalLb += `**${money.indexOf(money[i])+1}.**     <@${money[i].ID.slice(25)}> - \`${money[i].data} ZoraMoedas\`\n`;
+        }
+    
+        
+        const embed = new Discord.MessageEmbed()
+        .setAuthor(`${message.guild.name}`, message.guild.iconURL({ dynamic: true }))
+        .setColor("#ff0000")
+        .addField(`Leaderboards`, finalLb, false)
+        message.channel.send(embed);
+}
