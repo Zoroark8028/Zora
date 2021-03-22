9f20295325b90a034a90639bd8ea18b39ac7de4b
Glitch (zora) <none>
2021-03-22 15:50:41 +0000

    😎💗 Checkpoint ./commands/leaderboard.js:28980549/34 ./watch.json:28980549/4

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -5,11 +5,11 @@ module.exports.run = async (client, message, args) => {
 
  let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
 
-let money = db.all().filter(data => data.ID.startsWith(`money`)).sort((a, b) => b.data - a.data)
+let money = db.all().filter(data => data.ID.startsWith(`money_${message.guild.id}`)).sort((a, b) => b.data - a.data)
         money.length = 10;
         let finalLb = "";
         for (var i in money) {
-          finalLb += `**${money.indexOf(money[i])+1}.**     <@${money[i].ID.slice(25)}> - \`${money[i].data} ZoraMoedas\`\n`;
+          finalLb += `**${money.indexOf(money[i])+1}.**   <@${money[i].ID.slice(25)}> **-** \`${money[i].data} ZoraMoedas\`\n`;
         }
      
         
