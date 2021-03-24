397ba115d40180c9d537824d874b9425353bcba5
Glitch (zora) <none>
2021-03-24 21:15:36 +0000

    💵⛩ Checkpoint ./commands/z.r.js:28980549/33 ./watch.json:28980549/2

--- commands/z.r.js
+++ commands/z.r.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js')
 
 exports.run = async (bot, message, argumentos, arg_texto, chat) => {
-<:reverse:818301358598914059>
+
+message.channel.send('<:reverse:818301358598914059>')
+}
