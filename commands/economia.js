4052aa44bef057de9d27bbdd47ef44be9748c115
Glitch (zora) <none>
2021-03-26 20:09:30 +0000

    🍧👐 Checkpoint ./commands/economia.js:28980549/1172

--- commands/economia.js
+++ commands/economia.js
@@ -6,9 +6,9 @@ module.exports.run = async (client, message, args) => {
  .setColor("#ff0000")
  .setTitle("Moderação, o que os comandos fazem?")
  .setDescription(`
-**z.daily** \n Pegue uma quantia diaria  de ZoraMoedas que varie de **20-300** ZoraMoedas
 
-**z.atm** \n Verifica quantas zora
+
+c
 `)
 .setFooter('Os {} são argumentos! Não utilize os no comando!')
 
