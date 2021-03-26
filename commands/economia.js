9abb62654db317b5594a6d639e81dc5e899739e4
Glitch (zora) <none>
2021-03-26 20:20:21 +0000

    👛🐆 Checkpoint ./commands/economia.js:28980549/481 ./watch.json:28980549/4

--- commands/economia.js
+++ commands/economia.js
@@ -4,11 +4,16 @@ module.exports.run = async (client, message, args) => {
 
  const embed = new Discord.MessageEmbed()
  .setColor("#ff0000")
- .setTitle("Moderação, o que os comandos fazem?")
+ .setTitle("Economia, o que os comandos fazem?")
  .setDescription(`
 **z.daily** \n Pegue uma quantia diaria  de ZoraMoedas que varie de **20-300** ZoraMoedas
 
-**z.atm** \n Verifica quantas zora
+**z.atm** \n Verifica quantas ZoraMoedas **VOCÊ** tem
+
+**z.money** \n Verifica quantas ZoraMoedas a **PESSOA** mencionada tem
+
+**z.leaderboard** \n Mostra o ranking das pessoas que mais tem ZoraMoeda no server  
+
 `)
 .setFooter('Os {} são argumentos! Não utilize os no comando!')
 
