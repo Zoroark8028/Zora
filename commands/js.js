24908576baf0aef0e7df2802e0f32f0a56da022a
Glitch (zora) <none>
2021-04-05 06:02:17 +0000

    🎰🕴 Checkpoint ./commands/js.js:28980549/71 ./watch.json:28980549/6

--- commands/js.js
+++ commands/js.js
@@ -4,6 +4,6 @@ module.exports.run = async (client, message, args) => {
 
  const embed = new Discord.MessageEmbed()
  .setColor("#ff0000")
- .setTitle("Lista de comandos")
  .setDescription(`[aqui](https://zora.glitch.me)`)
- message.
+ message.channel.send(embed)
+  }
