1b920ec4ed595fdf256d8c80d08ee4592a8d1a5b
Glitch (zora) <none>
2021-04-06 02:22:30 +0000

    🐪🌷 Checkpoint ./commands/roll.js:28980549/133 ./watch.json:28980549/6

--- commands/roll.js
+++ commands/roll.js
@@ -1,7 +1,6 @@
 const Discord = require('discord-js')
 
-if (message.content.toLowerCase().includes("z.roll 100")) {
     var response = [Math.floor(Math.random() * ((100 - 1) + 1) + 1)];
 
-   message.channel.send("Você rolou o dado e conseguiu..." + response + "!").then().catch(console.error);  // "You got... 96!"
+   message.channel.send(`Você rolou o dado e conseguiu..." {response}!"`)
 }
