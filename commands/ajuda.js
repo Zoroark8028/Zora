39fa5684eb372e875526c31062d547c8626751fd
Glitch (zora) <none>
2021-04-06 01:31:05 +0000

    👢⏱ Checkpoint ./commands/ajuda.js:28980549/1276

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -1,11 +1,9 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-
- const embed = new Discord.MessageEmbed()
- .setColor("#ff0000")
- .setTitle("Lista de comandos")
- .setDescription(`
+  const embed = new Discord.MessageEmbed()
+    .setColor("#ff0000")
+    .setTitle("Lista de comandos").setDescription(`
 
 **Moderativos** - {z.moderação}
 <:hihihi:763836612114513930> ・ anuncio, sorteio 
@@ -18,8 +16,6 @@ module.exports.run = async (client, message, args) => {
 botinfo, serverinfo, avatar
 
 **Miscelânea**
-<:lanchando:760219665933795409> ・ suporte, convidar, zora, quiz`)
- message.channel.send(embed)
-
- }
-
+<:lanchando:760219665933795409> ・ suporte, convidar, zora, quiz`);
+  message.channel.send(embed);
+};
