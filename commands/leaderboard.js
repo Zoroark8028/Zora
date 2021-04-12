cc41dfbf59d10e9a5f215f6aafad66fe96e61026
Glitch (zora) <none>
2021-04-12 23:08:45 +0000

    Rewound to commit 62b830dde2883a88f54821a0b69fb0461e9719b6 + 0 edits

---
+++ commands/leaderboard.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+module.exports.run = async (client, message, args) => {
+
+ let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+
+let money = db.all().filter(data => data.ID.startsWith(`money_$`)).sort((a, b) => b.data - a.data)
+        money.length = 10;
+        let finalLb = "";
+        for (var i in money) {
+          finalLb += `**${money.indexOf(money[i])+1}.**   <@${money[i].ID.slice(25)}> **-** \`${money[i].data} ZoraMoedas\`\n`;
+        }
+     
+        
+        const embed = new Discord.MessageEmbed()
+        .setColor("#ff0000")
+        .addField(`<:lanchando:760219665933795409> **|** Top Burguêses de ZoraMoedas`, finalLb, false)
+        message.channel.send(embed);
+}
