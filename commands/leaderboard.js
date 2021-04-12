62b830dde2883a88f54821a0b69fb0461e9719b6
Glitch (zora) <none>
2021-04-12 03:16:35 +0000

    Rewound to commit 068f78d231d80cbd4cad0beba283d5cc07c62158 + 0 edits

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,19 +1,20 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
-const client = new Discord.Client(); 
+
 module.exports.run = async (client, message, args) => {
 
-let money = db.all().filter(data => data.ID.startsWith(`money`)).sort((a, b) => b.data - a.data)
+ let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+
+let money = db.all().filter(data => data.ID.startsWith(`money_$`)).sort((a, b) => b.data - a.data)
         money.length = 10;
         let finalLb = "";
         for (var i in money) {
-          finalLb += `**${money.indexOf(money[i])+1}.**     <@${money[i].ID.slice(25)}> - \`${money[i].data} \`\n`;
+          finalLb += `**${money.indexOf(money[i])+1}.**   <@${money[i].ID.slice(25)}> **-** \`${money[i].data} ZoraMoedas\`\n`;
         }
-        
+     
         
         const embed = new Discord.MessageEmbed()
-        .setAuthor(`${message.guild.name}`, message.guild.iconURL({ dynamic: true }))
         .setColor("#ff0000")
-        .addField(`Leaderboard`, finalLb, false)
+        .addField(`<:lanchando:760219665933795409> **|** Top Burguêses de ZoraMoedas`, finalLb, false)
         message.channel.send(embed);
 }
