2d1f8ca72989a56c25014f7c425d6b876e9392e2
Glitch (zora) <none>
2021-04-14 23:48:46 +0000

    🌱👩 Checkpoint ./commands/money.js:28980549/103 ./watch.json:28980549/4

--- commands/money.js
+++ commands/money.js
@@ -8,13 +8,9 @@ module.exports = {
     async run (client, message, args) {
 
 let user = message.mentions.members.first() ||  message.guild.members.cache.find(user => user.user.username.toLowerCase() === args.join(" ").toLowerCase()) ||
-    message.guild.members.cache.get(args[0]) || message.member;
+    message.guild.members.cache.get(args[0])
    
 if (!args[0]) {
-      return message.channel.send(`**Mencione um usuario!**`)
-}
-
-      if (!user) {
       return message.channel.send(`**Mencione um usuario!**`)
 }
 
