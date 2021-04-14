f76f690512469c89bc97d64fb67ef636b4bc7705
Glitch (zora) <none>
2021-04-14 22:51:04 +0000

    🏉🎣 Checkpoint ./package.json:28980549/1078 ./commands/leaderboard.js:28980549/2339

---
+++ commands/leaderboard.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const db = require('quick.db');
+module.exports.run = async (client, message, args) => {
+
+ let money = db.all().filter(data => data.ID.startsWith(`money`)).sort((a, b) => b.data - a.data)
+    money.length = 10;
+    var finalLb = "";
+    for (var i in money) {
+      finalLb += `**${money.indexOf(money[i])+1}. <@${client.users.cache.get(money[i].ID.split('_')[1]) ? client.users.cache.get(money[i].ID.split('_')[1]).tag : "Unknown User#0000"}>** - ${money[i].data} :dollar:\n`;
+    }
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(`Leaderboard!`, message.guild.iconURL())
+    .setColor("#7289da")
+    .setDescription(finalLb)
+    .setFooter(client.user.tag, client.user.displayAvatarURL())
+    .setTimestamp()
+    message.channel.send(embed);
+}
