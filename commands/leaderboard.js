46dbd6f76d1545c93e13af093eee284037180b38
Glitch (zora) <none>
2021-04-14 23:25:48 +0000

    🐦🎖 Checkpoint ./commands/leaderboard.js:28980549/3787 ./watch.json:28980549/4

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,19 +2,17 @@ const Discord = require("discord.js");
 const db = require('quick.db');
 module.exports.run = async (client, message, args) => {
 
-
-
- let money = db.all().filter(data => data.ID.startsWith(`money`)).sort((a, b) => b.data - a.data)
-        money.length = 10;
-        let finalLb = "";
-        for (var i in money) {
-          finalLb += `**${money.indexOf(money[i])+1}.** client.users.cache.get(money[i].ID.split('_')[1]) <@${money[i].ID.slice(25)}> - \`${money[i].data} \`\n`;
-        }
-        
-        
-        const embed = new Discord.MessageEmbed()
-        .setAuthor(`${message.guild.name}`, message.guild.iconURL({ dynamic: true }))
-        .setColor("#ff0000")
-        .addField(`Leaderboards`, finalLb, false)
-        message.channel.send(embed);
+   let money = db.all().filter(data => data.ID.startsWith(`money`)).sort((a, b) => b.data - a.data)
+    money.length = 10;
+    var finalLb = "";
+    for (var i in money) {
+      finalLb += `**${money.indexOf(money[i])+1}. <@${client.users.cache.get(money[i].ID.split('_')[1]) ? client.users.cache.get(money[i].ID.split('_')[1]).tag : "Unknown User#0000"}>** - ${money[i].data} :dollar:\n`;
+    }
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(`Leaderboard!`, message.guild.iconURL())
+    .setColor("#7289da")
+    .setDescription(finalLb)
+    .setFooter(client.user.tag, client.user.displayAvatarURL())
+    .setTimestamp()
+    message.channel.send(embed);
 }
