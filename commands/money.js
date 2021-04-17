b97c769575e357c7307bead71f9df5f76e78e4b6
Glitch (zora) <none>
2021-04-17 01:26:51 +0000

    🌝✊ Checkpoint ./commands/money.js:28980549/112 ./watch.json:28980549/2

--- commands/money.js
+++ commands/money.js
@@ -8,7 +8,7 @@ module.exports = {
     async run (client, message, args) {
 
 let user = message.mentions.members.first() ||  message.guild.members.cache.find(user => user.user.username.toLowerCase() === args.join(" ").toLowerCase()) ||
-    message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(user => user.user.nickname.toLowerCase() === args.join(" ").toLowerCase()) 
+    message.guild.members.cache.get(args[0])
    
 if (!args[0]) {
       return message.channel.send(`🔹 ${message.author}, se você quer ver **~~a sua~~** quantidade de ZoraMoedas, utilize **z.atm** `)
