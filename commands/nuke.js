ee7a416f1c21fc65ad0eaa164ca2c6009cf7d456
Glitch (zora) <none>
2021-04-17 03:38:21 +0000

    💁🏎 Checkpoint ./commands/nuke.js:28980549/244 ./watch.json:28980549/2

--- commands/nuke.js
+++ commands/nuke.js
@@ -2,9 +2,9 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: "nuke",
     description: "Nukes a given channel",
-    authorPermission: "ADMINISTRATOR",
     run: async(client, message, args) => {
-        let reason = args.join(" ") || "motivo não definido"
+     if(!message.member.permissions.has("ADMINISTRATOR"))return message.channel.send(`Somente pessoas com a permissão \`Administrador\` podem usar este comando!`)
+      let reason = args.join(" ") || "motivo não definido"
         if(!message.channel.deletable) {
             return message.reply("Esse canal não pode ser deletado!")
         }
