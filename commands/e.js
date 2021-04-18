25f09ca6f17012b8dddbf3134cf8410af051ab44
Glitch (zora) <none>
2021-04-18 20:18:03 +0000

    ⛺️📼 Checkpoint ./commands/e.js:28980549/1061 ./watch.json:28980549/8 ./commands/pagar.js:28980549/1677

--- commands/e.js
+++ commands/e.js
@@ -1,26 +1,25 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
- if(!['803363145921462272', '601473350619103235', '674656385656422411', '791639505757011969', '583408510184718336'].includes(message.author.id)) return;
- 
-    const help = new Discord.MessageEmbed()
+ if(!['803363145921462272'].includes(message.author.id)) return;
+     
+const help = new Discord.MessageEmbed()
    .setColor('BLUE')
 .setTitle('Utilize z.ajuda para ver meus comandos!')    
 .setDescription(`
     
- 🔹 **z.e**
-    \`Execute códigos dá libraria discord.js direto do discord!\`
+**🔹 z. **
+\`Execute códigos da libraria discord.js direto do discord!\`
 
-🔹 **Como Usar**    
+**🔹 Como Usar**    
 \`z.e {codigo}\`
 
-**Exemplos**
- 🔹 \`z.e message.channel.send('Olá')\`
+**🔹 Exemplos**
+\`z.e message.channel.send('Olá')\`
    
-**Permissões**
- 🔹 \`Dono do bot\`
+**🔹 Permissões**
+\`Dono Do Bot\` 
 `)
-      
       
    let conteudo = args.join(" ");
  
--- commands/pagar.js
+++ commands/pagar.js
@@ -4,55 +4,43 @@ const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, args, guilds) => {
   
- let user = message.mentions.users.first() || message.guild.members.cache.get(args[0])
+const help = new Discord.MessageEmbed()
+   .setColor('BLUE')
+.setTitle('Utilize z.ajuda para ver meus comandos!')    
+.setDescription(`
+    
+**🔹z.pagar**
+\`Dê Zoramoedas á alguém.\`
+
+**🔹 Como Usar**    
+\`z.pagar {usuario} {quantidade}\`
+
+**🔹 Exemplos**
+\`z.pagar @Zoroark#8028 1000\`
+    
+`)
+
+ let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) ||  message.guild.members.cache.find(user => user.user.username.toLowerCase() === args.join(" ").toLowerCase())
  
   let member = db.fetch(`money_${message.author.id}`)
 
-  let embed1 = new Discord.MessageEmbed()
-  .setColor("RED")
-  .setDescription(`**Mencione alguem!**`);
-
   if (!user) {
-      return message.channel.send(`🔹 ${message.author}, como paga o vento?`)
-
-}
-
-  let embed2 = new Discord.MessageEmbed()
-  .setColor("RED")
-  .setDescription(`Usagem Correta
-**z.pagar {menção} {quantidade}**`);
- 
+      return message.reply(help)
+  }
  if (!args[1]) {
-      return message.channel.send(`🔹 ${message.author}, esqueceu de utilizar o comando de forma certa, né!
-      \`z.pagar {menção} {quantidade}\``)
-}
-
+       return message.reply(help)
+ }
     const coinsToGive = args[1]
     if (isNaN(coinsToGive)) {
       message.channel.send(`🔹 ${message.author}, cd o número? nn sei`)
      return  
-}
-    let embed3 = new Discord.MessageEmbed()
-  .setColor("RED")
-  .setDescription(`*Você não pode usar dinheiro negativo!*`);
-
-
+    }
     if (message.content.includes('-')) { 
       return message.channel.send(`🔹 ${message.author}, você não pode usar dinheiro negativo, bobinho(a)!`)
-  }
-  let embed4 = new Discord.MessageEmbed()
-  .setColor("RED")
-  .setDescription(`*Você não tem ZoraMoedas o suficiente!*`);
-
+    }
   if (member < args[1]) {
       return message.channel.send(`🔹 ${message.author}, como você vai pagar alguém com algo que você não tem? `)
-   }
-  let embed5 = new Discord.MessageEmbed()
-  .setColor("GREEN")
-  .setDescription(`🔷 ・ **Sucesso**, **${message.author.tag}** transferiu **${args[1]} ZoraMoedas** para **${user}**!`);
- 
-
-
+  }
   message.channel.send(`🔹 **Sucesso**, **${message.author}**, você pagou **${args[1]} ZoraMoedas** para **${user}**!`)
   db.add(`money_${user.id}`, args[1])
   db.subtract(`money_${message.author.id}`, args[1])
