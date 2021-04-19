2ead7003947e7d001ffd027ae70d933647bed83d
Glitch (zora) <none>
2021-04-19 00:39:00 +0000

    🏔🍤 Checkpoint ./index.js:28980549/65 ./watch.json:28980549/4 ./package.json:28980549/1102

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ client.on('message', message => {
      if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
 
 client.aliases = new Discord.Collection();
-
-    const args = message.content
+client.commands = new Discord.Collection()
+  
+  const args = message.content
         .trim().slice(config.prefix.length)
         .split(/ +/g);
     const commandName = args.shift().toLowerCase();
