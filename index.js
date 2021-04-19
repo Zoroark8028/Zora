b69c4c372cdcbac5862ba12d3b9324c3884462c0
Glitch (zora) <none>
2021-04-19 00:36:37 +0000

    Rewound to commit 2a9b63e4bef65eaf1133b6ca082dc86b7531b067 + 0 edits

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ client.on('message', message => {
      if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
 
 client.aliases = new Discord.Collection();
-  client.commands = new Discord.Collection()
 
     const args = message.content
         .trim().slice(config.prefix.length)
