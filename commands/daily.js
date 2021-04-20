9cef2028ec10634fb02d4e2995240a4041403f74
Glitch (zora) <none>
2021-04-20 01:25:43 +0000

    🚛👞 Checkpoint ./commands/daily.js:28980549/210 ./watch.json:28980549/4 ./commands/e.js:28980549/22

--- commands/daily.js
+++ commands/daily.js
@@ -2,11 +2,8 @@ const Discord = require('discord.js')
 const db = require('quick.db');
 const ms = require('parse-ms');
 
-module.exports = {
-    name: "daily",
-    description: "Receive a daily award of money",
+module.exports.run = async (client, message, args) => {
 
-    async run (client, message, args) {
         let user = message.author;
         let timeout =  43200000;
         let amount = Math.floor(Math.random() * 300) + 20;
@@ -16,7 +13,7 @@ module.exports = {
         if(daily !== null && timeout - (Date.now() - daily) > 0){
             let time = ms(timeout - (Date.now() - daily));
 
-            return message.channel.send('<:742758255276720259:771911664172793856> **|** ${message.author}, você ja coletou seu daily de hoje, resgate denovo em: \`**${time.hours} horas , ${time.minutes} minutos e ${time.seconds} segundos**')
+            return message.channel.send(`<:742758255276720259:771911664172793856> **|** ${message.author}, você ja coletou seu daily de hoje, resgate denovo em: \`**${time.hours} horas , ${time.minutes} minutos e ${time.seconds} segundos**`)
         } else {
             db.add(`money_${user.id}`, amount);
             db.set(`daily_${user.id}`, Date.now());
@@ -24,4 +21,4 @@ module.exports = {
 message.channel.send(`<:742756936050671706:771911635924418590> **|** ${message.author}, você recebeu **${amount} ZoraMoedas** em seu daily!`)
         }
     }
-  }
+  
