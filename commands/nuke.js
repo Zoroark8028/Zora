be92ebfbfec71555e42cc55589fc2685c8c01c61
Glitch (zora) <none>
2021-04-20 01:36:59 +0000

    🍲🏣 Checkpoint ./commands/nuke.js:28980549/42 ./watch.json:28980549/2

--- commands/nuke.js
+++ commands/nuke.js
@@ -6,7 +6,7 @@ module.exports = {
      if(!message.member.permissions.has("ADMINISTRATOR"))return message.channel.send(`Somente pessoas com a permissão \`Administrador\` podem usar este comando!`)
       let reason = args.join(" ") || "motivo não definido"
         if(!message.channel.deletable) {
-            return message.reply("🔹 Esse canal não pode ser deletado!")
+            return message.reply("🔹 Esse canal não pode ser deletado!, talvez por que eu não tenho permissão.")
         }
         let newchannel = await message.channel.clone()
         await message.channel.delete()
