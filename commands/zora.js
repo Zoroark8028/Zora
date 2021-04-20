66ce7f0b1d69344a2ec09367e431fddedd366937
Glitch (zora) <none>
2021-04-20 19:47:39 +0000

    ❄️🌼 Checkpoint ./commands/zora.js:28980549/293 ./watch.json:28980549/4

--- commands/zora.js
+++ commands/zora.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
 
+let user = message.member;
+
   const help = new Discord.MessageEmbed()
    .setColor('BLUE')
 .setTitle('Utilize z.ajuda para ver meus comandos!')    
@@ -18,6 +20,7 @@ module.exports.run = async (client, message, args) => {
   
   let responses = [
         "<:hihihi:763836612114513930>・Sim",
+"",
         "<:Leon_Shark_Triste:807662645326905366>・Não",
         "<:Sandy_Joinha:807660320700760115>・Muito interresante",
         "<:lanchando:760219665933795409>・Que tal você ir estudar?",
@@ -25,12 +28,12 @@ module.exports.run = async (client, message, args) => {
 "<:Shelly_Bruxa_Feliz:807667755885133884>・Zora Aprova!",
    "<:El_Brown_Amor:807662132048035840>・Faz sentido.",
             "<:Rico_Assustado:807655918606090281>・hm, pensando bem, eu nunca pensei nisto!",
-"<:tatonamesa:758494052777197608>・Tome agua, Hidrata-se!","<:cat_toes_coca:830956888871010314>・Legal",'a'];
+"<:tatonamesa:758494052777197608>・..., serio?","<:cat_toes_coca:830956888871010314>・Legal",'a'];
       let response =
         responses[Math.floor(Math.random() * responses.length - 1)];
       const embed = new Discord.MessageEmbed()
-    
-      .setColor("#ff0000")
+ 
+      .setColor("BLUE")
  .setTitle("Zora Responde!")
  .setDescription(`Pergunta: **${args.join(' ')}**
 
