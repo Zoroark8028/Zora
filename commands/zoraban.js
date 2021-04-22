ba6b6af27b3f2eaeff94d6caabf6897696885f64
Glitch (zora) <none>
2021-04-22 02:25:09 +0000

    🐔🚋 Checkpoint ./commands/zoraunban.js:28980549/2340 ./watch.json:28980549/14 ./commands/zoraban.js:28980549/210

--- commands/zoraban.js
+++ commands/zoraban.js
@@ -2,15 +2,15 @@ const db = require("quick.db");
 exports.run = async(client, message, args) => {
    if(!['803363145921462272', '583408510184718336'].includes(message.author.id)) return;
 
-    let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author ||  message.guild.members.cache.find(user => user.user.username.toLowerCase() === args.join(" ").toLowerCase())
+    let user = message.mentions.users.first() || client.users.cache.get(args[0]) 
     if(!user) return message.channel.send(``);
     
     let fetched = db.get(`blacklist_${user.id}`)
     
     if(!fetched) {
       db.set(`blacklist_${user.id}`, true)
-      message.channel.send(`${user} entrou na blacklist, ninguem mandou quebrar as regras!`);
+      message.channel.send(`🔹 ${user} entrou na **blacklist**, ninguem mandou quebrar as regras, seu boboca!`);
     }else{ 
-      return message.channel.send(`esse usuario já está na blacklist.`);
+      return message.channel.send(`🔹 ${message.author}, esse usuario já está na blacklist.`);
     }
 }
--- commands/zoraunban.js
+++ commands/zoraunban.js
@@ -1,16 +1,15 @@
 const db = require("quick.db");
 exports.run = async(client, message, args) => {
-   if(!['803363145921462272', '583408510184718336'].includes(message.author.id)) return;
+ if(!['803363145921462272', '583408510184718336'].includes(message.author.id)) return;
 
-    let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author ||  message.guild.members.cache.find(user => user.user.username.toLowerCase() === args.join(" ").toLowerCase())
+    let user = message.mentions.users.first() || client.users.cache.get(args[0])
     if(!user) return message.channel.send(``);
-    
+  
     let fetched = db.get(`blacklist_${user.id}`)
-    
     if(!fetched) {
-       db.delete(`blacklist_${user.id}`)
-      message.channel.send(`${user} saiu da blacklist, não quebre as regras dessa vez!`);
-    }else{ 
-      return message.channel.send(`esse usuario não está na blacklist`);
+      return message.channel.send(`🔹 ${message.author}, Esse usuario não está na blacklist`);
+    }else{
+      db.delete(`blacklist_${user.id}`)
+      message.channel.send(`🔹 ${user}, saiu da blacklist, espero que não cometa nenhum erro na proxima vez!`)
     }
 }
