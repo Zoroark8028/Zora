8392a3e499efa3255eb61d6a7f6a356cf2d642e9
Glitch (zora) <none>
2021-04-23 01:30:45 +0000

    📒🏵 Checkpoint ./commands/informação/ping.js:28980549/628 ./watch.json:28980549/4

--- commands/informação/ping.js
+++ commands/informação/ping.js
@@ -1,9 +1,9 @@
 module.exports.run = async (client, message, args) => {
- 
   const m = await message.channel.send('Ping...');
-m.edit(`🏓 **| Pong!**\n🪀 **|** Latência do Server: **${m.createdTimestamp -
-      message.createdTimestamp}ms.**\n🎟️ **|** Latência da API: **${Math.round(
+
+  m.edit(`🏓 **| Pong!**\n:yo_yo: **|** Latência do Server: **${m.createdTimestamp -
+      message.createdTimestamp}ms.**\n:tickets: **|** Latência da API: **${Math.round(
       client.ws.ping
-    )}ms**
+    )}ms**`
   );
 };
