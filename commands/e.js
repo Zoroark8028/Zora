d7827538f3e20b4d87383f5484d80643c3d9a8fd
Glitch (zora) <none>
2021-04-23 20:44:40 +0000

    🍏🏦 Checkpoint ./commands/e.js:28980549/2971 ./watch.json:28980549/2

--- commands/e.js
+++ commands/e.js
@@ -1,12 +1,30 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-exports.run = async (client, message, args) => {
- if(!['803363145921462272', '601473350619103235', '674656385656422411', '791639505757011969', '583408510184718336'].includes(message.author.id)) return;
- 
- let conteudo = args.join(" ");
+module.exports = async (client, args, message) => {
+if(!['803363145921462272', '583408510184718336', '674656385656422411'].includes(message.author.id)) return;
+     
+const help = new Discord.MessageEmbed()
+   .setColor('BLUE')
+.setTitle('Utilize z.ajuda para ver meus comandos!')    
+.setDescription(`
+    
+**:small_blue_diamond: z.e **
+\`Execute códigos da libraria discord.js direto do discord!\`
+
+**:small_blue_diamond: Como Usar**    
+\`z.e {codigo}\`
+
+**:small_blue_diamond: Exemplos**
+\`z.e message.channel.send('Olá')\`
+   
+**:small_blue_diamond: Permissões**
+\`Dono Do Bot\` 
+`)
+      
+   let conteudo = args.join(" ");
  
  if(!conteudo) {
- return message.channel.send('**Coloque o código!**')
+ return message.channel.send(help)
  }
  
  let code = args.join(' ')
