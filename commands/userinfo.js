48bfc2e8ce6eaa2142e9a2406446187212a67176
Glitch (zora) <none>
2021-04-23 20:30:17 +0000

    🌅🎠 Checkpoint ./commands/userinfo.js:28980549/502

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -8,24 +8,6 @@ module.exports = {
   usage: "userinfo <MENTION>",
   description: "Get advance stats of given person or yourself",
   run: async (client, message, args) => {
-
-const flags = {
-	DISCORD_EMPLOYEE: 'Discord Employee',
-	DISCORD_PARTNER: 'Discord Partner',
-	BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
-	BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
-	HYPESQUAD_EVENTS: 'HypeSquad Events',
-	HOUSE_BRAVERY: 'House of Bravery',
-	HOUSE_BRILLIANCE: 'House of Brilliance',
-	HOUSE_BALANCE: 'House of Balance',
-	EARLY_SUPPORTER: 'Early Supporter',
-	TEAM_USER: 'Team User',
-	SYSTEM: 'System',
-	VERIFIED_BOT: 'Verified Bot',
-	VERIFIED_DEVELOPER: 'Verified Bot Developer'
-};
-
-    	
     
      let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
    
