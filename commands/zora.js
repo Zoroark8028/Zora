16f0690a8bab3017e0f577b129062bc279b7f2f8
Glitch (zora) <none>
2021-04-23 20:32:21 +0000

    🍯👖 Checkpoint ./commands/zora.js:28980549/3342 ./watch.json:28980549/2

--- commands/zora.js
+++ commands/zora.js
@@ -2,8 +2,26 @@ const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
 
+let user = message.member;
+
+  const help = new Discord.MessageEmbed()
+   .setColor('BLUE')
+.setTitle('Utilize z.ajuda para ver meus comandos!')    
+.setDescription(`
+    
+**🔹 z.zora**
+\`Pergunte coisas á mim, qual será minha reação?\`
+
+**🔹 Como Usar**    
+\`z.zora {pergunta}\`
+
+**🔹 Exemplos**
+\`z.zora você é um bot?\``)
+  
   let responses = [
+"<:cat_toes_coca:830956888871010314>・Tanto faz.",
         "<:hihihi:763836612114513930>・Sim",
+"<:sadcat4:830263376667607080>・essa pergunta me deixou triste ",
         "<:Leon_Shark_Triste:807662645326905366>・Não",
         "<:Sandy_Joinha:807660320700760115>・Muito interresante",
         "<:lanchando:760219665933795409>・Que tal você ir estudar?",
@@ -11,18 +29,20 @@ module.exports.run = async (client, message, args) => {
 "<:Shelly_Bruxa_Feliz:807667755885133884>・Zora Aprova!",
    "<:El_Brown_Amor:807662132048035840>・Faz sentido.",
             "<:Rico_Assustado:807655918606090281>・hm, pensando bem, eu nunca pensei nisto!",
-"<:tatonamesa:758494052777197608>・Tome agua, Hidrata-se!","<:cat_toes_coca:830956888871010314>・Legal",'a'];
+"<:tatonamesa:758494052777197608>・... serio?","<:cat_toes_coca:830956888871010314>・Legal",'a'];
       let response =
         responses[Math.floor(Math.random() * responses.length - 1)];
       const embed = new Discord.MessageEmbed()
     
       .setColor("#ff0000")
+ 
+      .setColor("BLUE")
  .setTitle("Zora Responde!")
  .setDescription(`Pergunta: **${args.join(' ')}**
 
 Resposta: **${response}**`)
-      .setFooter(`Autor(a) • ${message.author.tag}`)
-if(!args[0]) return message.reply("<:Bibi_Raiva:807663391144935484> | Por favor, coloque uma pergunta");
+      .setFooter(`Autor(a) • ${message.author.tag}`, message.author.displayAvatarURL({format: "png"}))
+if(!args[0]) return message.reply(help);
 
 message.channel.send(embed)
 }
