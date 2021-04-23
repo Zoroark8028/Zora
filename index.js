34c5859e3a74e8c245de84733cbda2c51edc03a8
Glitch (zora) <none>
2021-04-23 01:06:52 +0000

    Rewound to commit 06eaca4d1d50c943fdd6b75bba1280ab632f3fb1 + 0 edits

--- index.js
+++ index.js
@@ -58,17 +58,13 @@ client.on('message', message => {
 let user = db.get(`blacklist_${message.author.id}`);
   if(user == true) return;
 
-client.aliases = new Discord.Collection()
-client.commands = new Discord.Collection()
-
   const args = message.content
         .trim().slice(config.prefix.length)
         .split(/ +/g);
- const commandName = args.shift().toLowerCase();
-const command = client.commands.get(commandName);
-   
-  try {
-        command.execute(message, args);
+    const command = args.shift().toLowerCase();
+    try {
+        const commandFile = require(`./commands/${command}.js`)
+        commandFile.run(client, message, args);
     } catch (err) {
 console.error('Erro: ' + err);
      }
