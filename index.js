f21c50d82a3b7000b063c4fca5be9c3725618b46
Glitch (zora) <none>
2021-04-23 20:33:22 +0000

    🐀🐊 Checkpoint ./index.js:28980549/5843

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ const fs = require("fs")
 const db = require("quick.db")
 const bot = new Discord.Client();    
 
+client.on("guildMemberAdd", async (member) => { 
+
+  let guild = await client.guilds.cache.get("821563484617572352");
+  let channel = await client.channels.cache.get("821563484617572355")
+  if (guild != member.guild) {
+    return console.log("boas vindas!");
+   } else {
+channel.send(`**${member.user}** entrou no **Quartel da Zora**, vamos bater um papo com ele? Mas antes, não esqueça de **ler as regras!**`);
+  }
+});
+
 client.on("ready", () => {
 let avatar = [
 `https://cdn.glitch.com/93d6f45a-a737-47e8-8966-31894015e729%2F9a79cc19-82f8-4c35-9772-2009aba4e4d1.image.png?v=1614373229732`,  
@@ -43,8 +54,12 @@ client.on('message', message => {
      if (message.channel.type == 'dm') return;
      if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
      if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+     if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return
+  
+let user = db.get(`blacklist_${message.author.id}`);
+  if(user == true) return;
 
-    const args = message.content
+  const args = message.content
         .trim().slice(config.prefix.length)
         .split(/ +/g);
     const command = args.shift().toLowerCase();
@@ -53,10 +68,13 @@ client.on('message', message => {
         const commandFile = require(`./commands/${command}.js`)
         commandFile.run(client, message, args);
     } catch (err) {
+console.error('Erro: ' + err);
      }
 });
 
 client.on('message', message => {
+        
+  client.on('message', message => {
     if (message.content === '<@!803373957738528778>') {
         message.channel.send(':zap: ・ Oie, meu prefixo é **z.** , se precisar de qualquer ajuda, utilize **z.ajuda**');
     }
@@ -70,3 +88,4 @@ client.on('message', message => {
 
 client.login(process.env.TOKEN); 
 console.log(`[CONECTADA] Zora Natasha#4439 foi conectada com sucesso ao Discord.`)  
+})
